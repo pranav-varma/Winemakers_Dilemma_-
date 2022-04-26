475340951fe95c131d2a2beb7938923b28f769fd
pranav01varma <pranav01varma@gmail.com>
2022-04-25 21:06:28 -0400

    code for the app

---
+++ code.py
@@ -0,0 +1,30 @@
+def calculate_e_value(sensitivity, specificity, PNS, Harvest_value=1000, Wait_Harvest_NS=1500, Wait_Harvest_S=500):
+    # Probability of detected No Storm
+    PDNS = specificity*PNS + (1-sensitivity)*(1-PNS)
+    print("PDNS =",PDNS)
+    
+    # Probability of No Storm given Detected No Storm
+    P_DNS_NS = specificity*PNS / PDNS
+    print("P_DNS_NS =",P_DNS_NS)
+    
+    # Probability of Detected Storm
+    PDS = 1 - PDNS
+    
+    eValue = PDNS * ( (Wait_Harvest_NS * P_DNS_NS) + (Wait_Harvest_S * (1 - P_DNS_NS)) ) + \
+             PDS  * (Harvest_value)
+    print("E Value =",eValue)
+    
+    if(eValue < Harvest_value):
+        print("Recommended Action : Harvest Now")
+    
+    P_S = 1- PNS
+    P_DS_S = sensitivity*P_S / PDS
+    print("P_DS_S =",P_DS_S)
+
+if __name__ == "__main__":
+    calculate_e_value(sensitivity = 0.76,
+                  specificity = 0.69, 
+                  PNS = 0.25, 
+                  Harvest_value = 1000, 
+                  Wait_Harvest_NS = 1500, 
+                  Wait_Harvest_S = 500)
